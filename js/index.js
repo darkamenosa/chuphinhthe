d669915260c984f7740c32b1a4cbb53f91b12dfb
tuyenhx <hxtxmu@gmail.com>
2018-06-13 23:17:27 +0700

    update async load img

--- js/index.js
+++ js/index.js
@@ -1,10 +1,20 @@
 $(document).ready(function() {
 
+    function handleLazyLoadImg() {
+      $('.lazy-load').each(function() {
+        // Set src attribute of current element by value of data-src 
+        var currentElement = $(this);
+        var src            = currentElement.attr('data-src');
+        currentElement.attr('src', src); 
+      })
+    }
+
     function onActionBtnClicked(event) { 
       window.location.href = "http://dangky.chuphinhthe.com"; 
     }
 
     function main() {
+      handleLazyLoadImg();
       $('.btn-action').on('click touchstart', onActionBtnClicked);
     }
 
